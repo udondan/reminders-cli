b3b83cc7e384386db52887dfd7b3a01d63b782cc
Bill Cromie <bill.cromie@gmail.com>
2024-10-25 14:05:29 -0400

    feat: accept a list id in place of a list name for add
    addReminder's list argument now resolves either a list name or its calendarIdentifier.
    
    Cherry-picked from keith/reminders-cli#90, commit 1f7ac31b ("can create reminders using uuids").

--- Sources/RemindersLibrary/CLI.swift
+++ Sources/RemindersLibrary/CLI.swift
@@ -255,7 +255,7 @@ private struct Add: ParsableCommand {
         reminders.addReminder(
             string: self.reminder.joined(separator: " "),
             notes: self.notes,
-            toListNamed: self.listNameOrId,
+            toListNameOrId: self.listNameOrId,
             dueDateComponents: self.dueDate,
             priority: priority,
             recurrence: self.repeat_,
